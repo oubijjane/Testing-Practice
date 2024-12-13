472d2f9b2cc6f10d00854fa707258926b5ea45de
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-12-13 15:43:29 +0100

    feat: add substract devid and multiply operations to calculatior with unit testing feat: add caesarCipher(string, key) with unit testing

--- main.js
+++ main.js
@@ -2,16 +2,52 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase();
 }
 function reverseString(string) {
-    let result = ""
-    for(let i = string.length - 1; i >= 0; i--) {
-        result += string.charAt(i)
-    }
-    return result;
+  let result = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string.charAt(i);
+  }
+  return result;
 }
 class Calculator {
- add(num1, num2) {
+  add(num1, num2) {
     return num1 + num2;
- }
+  }
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+  divide(num1, num2) {
+    return num1 / num2;
+  }
 }
 
-export { capitalize, reverseString, Calculator };
+function caesarCipher(string, key) {
+    if(key > 26) {
+        key = key % 26;
+    }
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    let cipheredCharNUm = string.charCodeAt(i);
+    if (cipheredCharNUm >= 97 && cipheredCharNUm <= 122) {
+      cipheredCharNUm += key;
+      if(cipheredCharNUm > 122) {
+        cipheredCharNUm -= 26;
+      }
+    } else if (cipheredCharNUm >= 65 && cipheredCharNUm <= 90) {
+        cipheredCharNUm += key;
+        if(cipheredCharNUm > 90) {
+          cipheredCharNUm -= 26;
+        }
+    }
+    result += String.fromCharCode(cipheredCharNUm);
+  }
+  /* helperMethode(nume, key); */
+  console.log(result);
+  return result;
+}
+caesarCipher("xyz", 3);
+console.log(1000 % 26);
+console.log("1".charCodeAt(0));
+export { capitalize, reverseString, Calculator, caesarCipher };
--- main.test.js
+++ main.test.js
@@ -1,19 +1,59 @@
-
-import { capitalize,reverseString, Calculator } from "./main.js";
-
+import { capitalize, reverseString, Calculator, caesarCipher } from "./main.js";
 
 test("first character from a string to be capitalized", () => {
-    expect(capitalize("test")).toBe("T");
-    expect(capitalize("hello")).toBe("H");
-}) 
+  expect(capitalize("test")).toBe("T");
+  expect(capitalize("hello")).toBe("H");
+});
 
 test("reversing a string", () => {
-    expect(reverseString("hello")).toBe("olleh");
-    expect(reverseString("thanks")).toBe("sknaht");
-})
+  expect(reverseString("hello")).toBe("olleh");
+  expect(reverseString("thanks")).toBe("sknaht");
+});
 
 test("adding operation", () => {
-    const calculator = new Calculator()
-    expect(calculator.add(2,2)).toBe(4);
-    expect(calculator.add(0,2)).toBe(2);
-})
+  const calculator = new Calculator();
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.add(0, 2)).toBe(2);
+});
+
+test("subtracting operation", () => {
+  const calculator = new Calculator();
+  expect(calculator.subtract(2, 2)).toBe(0);
+  expect(calculator.subtract(0, 2)).toBe(-2);
+});
+
+test("multiplying operation", () => {
+  const calculator = new Calculator();
+  expect(calculator.multiply(3, 2)).toBe(6);
+  expect(calculator.multiply(0, 2)).toBe(0);
+});
+
+test("multiplying operation", () => {
+  const calculator = new Calculator();
+  expect(calculator.multiply(3, 2)).toBe(6);
+  expect(calculator.multiply(0, 2)).toBe(0);
+});
+test("deviding operation", () => {
+  const calculator = new Calculator();
+  expect(calculator.divide(3, 2)).toBeCloseTo(1.5);
+  expect(calculator.divide(0, 2)).toBeCloseTo(0);
+});
+
+test("Shift Cipher lower and upper case", () => {
+  expect(caesarCipher("abc", 3)).toBe("def");
+  expect(caesarCipher("ABC", 2)).toBe("CDE");
+});
+test("Shift Cipher keep the same capitalization", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("XYZ", 3)).toBe("ABC");
+});
+
+test("Shift Cipher large key", () => {
+  expect(caesarCipher("abc", 29)).toBe("def");
+  expect(caesarCipher("XYZ", 29)).toBe("ABC");
+});
+
+test("Shift Cipher with non alphabets", () => {
+  expect(caesarCipher("Hello, World!", 29)).toBe("Khoor, Zruog!");
+  expect(caesarCipher("Hello!", 29)).toBe("Khoor!");
+});
